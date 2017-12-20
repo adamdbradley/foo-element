06707a9cccb72b79c0ecd67e771f233c31b861d0
Adam Bradley <adambradley25@gmail.com>
2017-12-19 21:15:58 -0600

    quick hack

--- src/components/foo-element/foo-element.tsx
+++ src/components/foo-element/foo-element.tsx
@@ -1,5 +1,23 @@
 import { Component, State } from '@stencil/core';
-import '../web-components/ce-with-event.js';
+
+class CEWithEvent extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.innerHTML = `<p>hello from ce</p>`;
+    this.addEventListener('click', this.onClick);
+  }
+  onClick() {
+    this.dispatchEvent(new CustomEvent('lowercaseevent'));
+    this.dispatchEvent(new CustomEvent('kebab-event'));
+    this.dispatchEvent(new CustomEvent('camelEvent'));
+    this.dispatchEvent(new CustomEvent('CAPSevent'));
+    this.dispatchEvent(new CustomEvent('PascalEvent'));
+  }
+}
+
+customElements.define('ce-with-event', CEWithEvent);
+
 
 @Component({
   tag: 'foo-element',
